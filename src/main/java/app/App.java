ab895fccc482ff821f5f41ba4fa7dfcd0fc09c3f
genenrajh.us <krev.ulyana@yandex.ru>
2024-03-28 17:06:19 +0300

    realization of the card

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,7 +1,10 @@
 package app;
 
+import bank.Card;
+
 public class App {
     public static void main(String[] args) {
-
+        Card card=new Card();
+        System.out.println(card.getPinCode());
     }
 }
--- src/main/java/bank/Card.java
+++ src/main/java/bank/Card.java
@@ -1,4 +1,31 @@
 package bank;
 
+import java.util.Random;
+
 public class Card {
-}
+        private final String CARD_NUMBER = "1234 5678 9012 3456";
+        private int pinCode;
+
+        public Card() {
+            this.pinCode = generatePinCode();
+        }
+
+        public int getPinCode() {
+            return pinCode;
+        }
+
+        public void setPinCode(int pinCode) {
+            if (pinCode >= 1000 && pinCode <= 9999) {
+                this.pinCode = pinCode;
+            } else {
+                System.out.println("Pin code must be a four-digit number.");
+            }
+        }
+
+        private int generatePinCode() {
+            Random random = new Random();
+            return random.nextInt(9000) + 1000;
+        }
+
+        // Testing the Card class
+    }
